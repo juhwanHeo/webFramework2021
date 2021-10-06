f745c50387c9115578c574572fcbde97c76d5ace
juhwanHeo <juhwanheo.dev@gmail.com>
2021-10-06 16:35:33 +0900

    21.10.06 - 1 (desktop)
    1, 수행괴제(6) 해결
      - 5주차 대면 실습문제를 @Autowired를 사용하기

---
+++ web-spring-2021-test/src/main/java/com/week6/task/p01/HaksaService.java
@@ -0,0 +1,8 @@
+package com.week6.task.p01;
+
+public interface HaksaService {
+	double sum();
+	double avg();
+	void print();
+	
+}
---
+++ web-spring-2021-test/src/main/java/com/week6/task/p01/HaksaServiceImpl.java
@@ -0,0 +1,34 @@
+package com.week6.task.p01;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class HaksaServiceImpl implements HaksaService {
+	
+	private Student student;
+	
+	@Autowired
+	@Qualifier("heojuhwan")
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	@Override
+	public double sum() {
+		return student.getMiddleScore() + student.getFinalScore();
+	}
+
+	@Override
+	public double avg() {
+		return sum() / 2;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("이름: " + student.getName());
+		System.out.println("총합: " + sum());
+		System.out.println("평균: " + avg());
+
+	}
+
+}
